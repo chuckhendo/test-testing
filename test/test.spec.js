19dc4808eca4c0c9afb05ddc8acfe5cda10a183d
Anthony Henderson <chuckhendo@gmail.com>
2015-12-07 22:06:45 -0500

    updated test

--- test/test.spec.js
+++ test/test.spec.js
@@ -3,5 +3,5 @@ var expect = require('chai').expect;
 describe("A test suite", function() {
    beforeEach(function() { });
    afterEach(function() { });
-   it('should fail', function() { expect(Array.from).to.exist; });
+   it('should fail', function() { expect(true).to.be.true; });
 });
